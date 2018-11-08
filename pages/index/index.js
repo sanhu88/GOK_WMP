8bcebd9810617f767975abe2fd055dbd5eb031ff
Burt Zheng <504779282@qq.com>
2018-11-08 21:58:20 +0800

    use let get the value from result

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,9 +8,15 @@ Page({
       
       /*success(res) {*/
       success: res =>{
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp,weather)
+        /*console.log(res.data.result.now.temp)
+        console.log(res.data.result.now.weather)
         console.log(res.data)
         console.log(res.statusCode)
-        console.log(res.header)
+        console.log(res.header)*/
       }
     })
    }
