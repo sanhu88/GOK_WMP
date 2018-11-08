9a6f0831a932ec57e7bcc9761ee1728425e9e3b1
Burt Zheng <504779282@qq.com>
2018-11-08 21:41:28 +0800

    add  wx.request on index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,18 @@
 Page({
   onLoad() {
-    console.log('Hello World!')
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: '广州市'
+      },
+      
+      /*success(res) {*/
+      success: res =>{
+        console.log(res.data)
+        console.log(res.statusCode)
+        console.log(res.header)
+      }
+    })
    }
 
 })
