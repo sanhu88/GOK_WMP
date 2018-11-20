05c5f091e60b25582c830cd16217189c27f759c3
Burt Zheng <504779282@qq.com>
2018-11-20 23:10:01 +0800

    update index.js 逗号分号

--- pages/index/index.js
+++ pages/index/index.js
@@ -23,14 +23,16 @@ Page({
     nowTemp :'',
     nowWeather : '',
     nowWeatherBackground:'',
-    hourlyWeather: []
+    hourlyWeather: [],
+    todayTemp: "",
+    todayDate: "",
   },
   onLoad() {
-    this.getNow()
+    this.getNow();
   }, 
   onPullDownRefresh() {
     this.getNow(() => {
-      wx.stopPullDownRefresh()
+      wx.stopPullDownRefresh();
     })
   },
   getNow(callback){
@@ -38,7 +40,7 @@ Page({
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
       data: {
-        city: '广州市'
+        city: '广州市',
       },
       
       /*success(res) {*/
@@ -58,20 +60,20 @@ Page({
 
       /**start complete */
       complete: () => {
-        callback && callback()
+        callback && callback();
       }   /**end complete */
     })
    },
    setNow(result){
-     let temp = result.now.temp
-     let weather = result.now.weather
+     let temp = result.now.temp;
+     let weather = result.now.weather;
      /*let forecast = result.forecast*/
      /**console.log(forecast)*/
      /*console.log(temp,weather)*/
      this.setData({
        nowTemp: temp + '°c',
        nowWeather: weatherMap[weather],
-       nowWeatherBackground: '/images/' + weather + '-bg.png'
+       nowWeatherBackground: '/images/' + weather + '-bg.png',
 
      }),/**end setData */
        wx.setNavigationBarColor({
@@ -92,9 +94,9 @@ Page({
         temp: forecast[i].temp + "°",
       })
     }
-    hourlyWeather[0].time = '现在'
+    hourlyWeather[0].time = '现在',
     this.setData({
-      hourlyWeather: hourlyWeather
+      hourlyWeather: hourlyWeather,
     })
   },
 
